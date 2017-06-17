696bfff91d87d1a92542acadd86086de52aedea3
Mati <matias.halperin@gmail.com>
2017-06-16 22:34:05 -0300

    AOSCP bringup

--- aoscp.mk
+++ aoscp.mk
@@ -21,10 +21,11 @@ TARGET_SCREEN_HEIGHT := 960
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := aoscp_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
+PRODUCT_MODEL := surnia
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto E LTE (2nd gen)"
